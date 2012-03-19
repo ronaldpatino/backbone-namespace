ef99362a46bd724b205c7bade60dce90edee7c25
ronaldpatino <ronaldpatino@gmail.com>
2012-03-19 17:30:58 -0500

    Adding show user detail screen

---
+++ app/views/user/show_view.js
@@ -0,0 +1,18 @@
+App.view.user = App.view.user || {}
+
+App.view.user.show = Backbone.View.extend({
+
+    tagName: 'ul',
+
+    initialize: function(options) {
+        console.log('App.view.user.user Initialized');
+        this.template = _.template(App.util.templates.get('user/show'));
+    },
+
+    render:function () {
+        $(this.el).html(this.template(this.model.toJSON()));
+        return this;
+    }
+});
+
+
