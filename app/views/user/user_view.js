37604124dce12a39645b5ac68fd57315a31067e8
ronaldpatino <ronaldpatino@gmail.com>
2012-03-19 17:37:39 -0500

    Added events to view

--- app/views/user/user_view.js
+++ app/views/user/user_view.js
@@ -4,18 +4,22 @@ App.view.user.user = Backbone.View.extend({
 
     tagName: 'li',
 
-    initialize: function(options) {
+    initialize: function (options) {
         console.log('App.view.user.user Initialized');
         this.template = _.template(App.util.templates.get('user/user'));
     },
 
-    render:function () {
+    events: {
+        "click :li": 'alerta'
+    },
+
+    render: function () {
         $(this.el).html(this.template(this.model.toJSON()));
         return this;
     },
 
-    events: {
-
+    alerta: function () {
+        console.log('alerta');
     }
 });
 
