cbbd0f5a64e25623d477162a8ab27d8cfbf01aae
ronaldpatino <ronaldpatino@gmail.com>
2012-03-19 17:30:31 -0500

    moved includes to a separate file

---
+++ app/includes.js
@@ -0,0 +1,29 @@
+head.js(
+'app/lib/jquery.js',
+'assets/js/bootstrap-transition.js',
+'assets/js/bootstrap-alert.js',
+'assets/js/bootstrap-modal.js',
+'assets/js/bootstrap-dropdown.js',
+'assets/js/bootstrap-scrollspy.js',
+'assets/js/bootstrap-tab.js',
+'assets/js/bootstrap-tooltip.js',
+'assets/js/bootstrap-popover.js',
+'assets/js/bootstrap-button.js',
+'assets/js/bootstrap-collapse.js',
+'assets/js/bootstrap-carousel.js',
+'assets/js/bootstrap-typeahead.js',
+'app/lib/underscore.js',
+'app/lib/backbone.js',
+'app/lib/backbone.localstorage.js',
+'app/app.js',
+'app/util.js',
+'app/router.js',
+'app/models/user/user_model.js',
+'app/collections/user/user_collection.js',
+'app/views/user/user_view.js',
+'app/views/user/list_view.js',
+'app/views/user/show_view.js',
+function() {
+    App.init();
+});
+
