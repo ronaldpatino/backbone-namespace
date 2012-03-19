0b2796e8f65c0d7de33320b5f66564b513b2f4d9
ronaldpatino <ronaldpatino@gmail.com>
2012-03-19 17:32:08 -0500

    Adding show user detail screen

--- app/app.js
+++ app/app.js
@@ -8,7 +8,11 @@ window.App = {
 
     init: function() {
         console.log('window.App Initialized')
-        App.util.templates.loadTemplates(['user/list','user/user'],
+        App.util.templates.loadTemplates([
+                                            'user/list',
+                                            'user/user',
+                                            'user/show'
+                                        ],
             function () {
                 new App.router.user();
                 Backbone.history.start();
