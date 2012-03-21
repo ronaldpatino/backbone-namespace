fae18f6c218bda5fae0d4a7feae4c9034971d7bd
ronaldpatino <ronaldpatino@gmail.com>
2012-03-21 12:16:06 -0500

    added JST namespace and removed async loading of .html templates

--- app/app.js
+++ app/app.js
@@ -5,18 +5,12 @@ window.App = {
     router:  {},
     util: {},
     data: {},
+    JST:{},
     userCollection: null,
 
     init: function() {
         console.log('window.App Initialized')
-        App.util.templates.loadTemplates([
-                                            'user/user',
-                                            'user/show',
-                                            'message/error'
-                                        ],
-            function () {
-                new App.router.user();
-                Backbone.history.start();
-            });
+        new App.router.user();
+        Backbone.history.start();
     }
 }
