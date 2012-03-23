cb9c5175965f71834d1cfa7a73cb19f1aedc0efe
ronaldpatino <ronaldpatino@gmail.com>
2012-03-23 17:15:43 -0500

    added tempalte to view

--- app/views/user/list_view.js
+++ app/views/user/list_view.js
@@ -2,15 +2,22 @@ App.view.user = App.view.user || {}
 
 App.view.user.list = Backbone.View.extend({
 
-        tagName: 'ul',
+       tagName: 'span',
 
         initialize: function(options) {
             console.log('App.view.user.list Initialized');
+            this.template = App.JST['user\list'];
         },
 
         render:function () {
+
+            // Set the template for the list_view component
+            $(this.el).html(this.template());
+
+            // Add each user to the list
             _.each(this.model, function (user) {
-                $(this.el).append(new App.view.user.user({model:user}).render().el);
+                $('#list_view', this.el).append(new App.view.user.user({model:user}).render().el);
+
             }, this);
 
             return this;
